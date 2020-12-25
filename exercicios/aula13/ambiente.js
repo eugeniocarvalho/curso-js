15cf23b59e813de6e10b7be6e761ea761117c0c7
Eugênio Carvalho <eugenio9008@gmail.com>
2020-12-25 19:46:44 -0300

    initial commit

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,16 @@
+let c = 0
+
+console.log('Usando While\n')
+
+while (c < 6) {
+  console.log('Fala galera')
+  c++
+}
+
+console.log('\nUsando Do While\n')
+c = 0
+
+do {
+  console.log('Faaaala galera')
+  c++
+} while (c < 6)
