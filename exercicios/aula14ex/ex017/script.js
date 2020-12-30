89d18e207c218778123763648e9861f77eb45b9c
Eugênio Carvalho <eugenio9008@gmail.com>
2020-12-30 17:15:36 -0300

    output treatment

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,10 +1,10 @@
 function gerarTabuada() {
   const multiplicador = Number.parseInt(document.getElementById('multiplicador').value)
-  res = document.querySelector('#res')
+  const res = document.querySelector('#res')
 
   if (multiplicador) {
     res.value = ''
-    for (let i = 0; i < 10; i++)
+    for (let i = 0; i <= 10; i++)
       res.value += `${multiplicador} x ${i} = ${multiplicador * i}\n`
   }
   else {
